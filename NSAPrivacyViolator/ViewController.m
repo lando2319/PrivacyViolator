48b82d43ebc1a42a138f5fd58a0b9ea56b80ff48
Mike Land <mikepland@gmail.com>
2014-10-15 11:31:23 -0500

    sets up CLLocation

--- NSAPrivacyViolator/ViewController.m
+++ NSAPrivacyViolator/ViewController.m
@@ -7,24 +7,31 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet UITextView *myTextView;
+@property CLLocationManager *myLocationManager;
 
 @end
 
 @implementation ViewController
 - (IBAction)startViolatingPrivacy:(id)sender {
+    [self.myLocationManager startUpdatingLocation];
+    self.myTextView.text = @"Locating You...";
     NSLog(@"toast");
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.myLocationManager = [[CLLocationManager alloc] init];
+    [self.myLocationManager requestWhenInUseAuthorization];
+    self.myLocationManager.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"I failed: %@", error);
 }
+
 
 @end
